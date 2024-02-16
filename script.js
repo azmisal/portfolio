5c5b56e31d530297f61dde1cf0fea8a302d43967
azmisal <b20cs33@gectcr.ac.in>
2024-02-17 00:31:31 +0530

    almost done

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ document.getElementById('cvDownload').addEventListener('click', function () {
     link.click();
 });
   
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    alert("Form submitted!"); 
+});
